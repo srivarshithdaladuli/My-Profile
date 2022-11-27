68bc17f36a91eada48a5e6cfa5ada0c08f4b7606
Srivarshith Daladuli <44353450+srivarshithdaladuli@users.noreply.github.com>
2022-11-27 10:27:17 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'vsrivarshith@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -18,7 +18,7 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
+  $contact->to = $vsrivarshith@gmail.com;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
